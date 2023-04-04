99086855ce56fd4226fdd2377c784ad17f8caf5e
Brian van den Berg <brianvdberg2107@gmail.com>
2023-04-04 16:53:29 +0300

    Working express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+import app from './app';
+require('dotenv').config();
+
+// Get/Set the listening port
+const port = process.env.PORT || 3000;
+
+// Main function
+const main = async () => {
+  try {
+    // Start listening to the port
+    app.listen(port, () => {
+      console.log(`Listening: http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error((err as Error).message);
+  }
+};
+
+// Execute the main
+main();
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,25 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
 import CustomError from './classes/CustomError';
-import ErrorResponse from './interfaces/ErrorResponse';
 
 // Not found handler
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-    const error = new CustomError(`Not Found - ${req.originalUrl}`, 404);
-    next(error);
+    res.status(404).end();
 };
 
 // Error handler
 const errorHandler = (
     err: CustomError,
     req: Request,
-    res: Response<ErrorResponse>,
+    res: Response,
     next: NextFunction
   ) => {
     console.error('errorHandler', err.message);
     res.status(err.status || 500);
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? 'Something went wrong' : err.stack,
+        stack: process.env.NODE_ENV === 'production' ? 'Not in production' : err.stack,
     });
 };
 
