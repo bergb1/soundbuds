5890c0710f3fed5e3857d92f08dfd440d8c2a826
Brian van den Berg <brianvdberg2107@gmail.com>
2023-04-17 10:22:22 +0300

    Added app configurations

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,32 @@
 import express, { Express } from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import { notFound, errorHandler } from './middlewares';
 
 // Configuration for Express
 const conf_app = async (app: Express) => {
     try {
-        // Configure helmet
-        app.use(
-            helmet({
-                crossOriginEmbedderPolicy: false,
-                contentSecurityPolicy: false
-            })
-        );
-
         app.use(notFound);
         app.use(errorHandler);
-
     } catch (err) {
-        console.error((err as Error).message);
+        console.log(err);
     }
 }
 
 // Perform the configuration
 const app = express();
+
+// Configure middleware
+app.use(
+    helmet({
+        crossOriginEmbedderPolicy: false,
+        contentSecurityPolicy: false
+    })
+);
+app.use(cors<cors.CorsRequest>());
+app.use(express.json());
+
+// Configure the app
 conf_app(app);
 
 export default app;
