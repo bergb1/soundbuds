ba9db47e5bcc2f61da63aa173283a62b138067b2
Brian van den Berg <brianvdberg2107@gmail.com>
2023-04-27 18:23:24 +0300

    Removed an import

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import uploadRoute from './routes/uploadRoute';
-import MessageResponse from '../interfaces/MessageResponse';
 
 // Create a router
 const router = express.Router();
